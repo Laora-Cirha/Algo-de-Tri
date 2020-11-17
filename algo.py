2797bf85783c4261d64019586d0ce2dd74b144c7
Laora-Cirha <73877056+Laora-Cirha@users.noreply.github.com>
2020-11-17 10:24:22 +0100

    Update algo.py

--- algo.py
+++ algo.py
@@ -17,3 +17,47 @@ def triSelect (list):
     return result
 print("list trié :")
 print(triSelect(list))
+
+# ---------------------------------------
+# ---------------------------------------
+
+"""def triFusion(list):
+    len(list)
+    if len(list)>0:
+        list[/2]
+        
+        
+       
+        
+        liste0 = partie gauche de la liste
+        list1 = partie droite de la liste
+
+        list0Trie = triFusion(list0)
+        list1Trie = triFusion(list1)
+        return fusion (list0trie,list1trie)
+        
+    else return list
+"""
+# --------------------------------------
+#   Démineur
+# --------------------------------------
+
+
+
+def _init_(hauteur, largeur):
+
+    listOflist=[]
+
+    for i in range(hauteur):
+        listRow=[]
+
+        for j in range(largeur):
+            listRow.append("x")
+        listOflist.append(listRow)
+
+    return listOflist
+
+liste=_init_(5,5)
+
+for i in liste:
+    print(i)
