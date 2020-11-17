347fe9ec80f368f4f58882375d4568ece5b5f4a1
Laora-Cirha <73877056+Laora-Cirha@users.noreply.github.com>
2020-11-17 09:10:57 +0100

    Update algo.py

--- algo.py
+++ algo.py
@@ -1,1 +1,19 @@
+list=[4,2,7,8]
+def selectMin(list):
+    var=list[0]
+    for i in list:
+        if var>i:
+            var=i
+    return var
+print ("liste de base :")
+print(list)
 
+def triSelect (list):
+    result=[]
+    while len(list)>0:
+        var=selectMin(list)
+        list.remove(var)
+        result.append(var)        
+    return result
+print("list trié :")
+print(triSelect(list))
