e841b0ad1222e7f5de8debde712575e66b9e22ed
Laora-Cirha <73877056+Laora-Cirha@users.noreply.github.com>
2020-11-24 11:50:42 +0100

    Update Démineur.py

--- Démineur.py
+++ Démineur.py
@@ -2,15 +2,30 @@
 """   Démineur """
 # --------------------------------------
 
-# --------------------------------------
-"""   Tableau visible """
-# --------------------------------------
+# Variables
+# Modifiez ces variables pour changer les settings du jeu
+
+import random
 
 
 hauteur = 5
 largeur = 5
+minesMax = 5
 
-import random
+# --------------------------------------
+"""   Nombre de mines """
+# --------------------------------------
+
+def mines(max):
+    nMine = random.randint(2,minesMax)
+    return nMine
+print("Il y a |",mines(minesMax),"| mines.")
+
+# --------------------------------------
+"""   Tableau visible """
+# --------------------------------------
+
+# Paramètre de la grille
 def _init_(hauteur, largeur):
 
     listOflist=[]
@@ -24,16 +39,19 @@ def _init_(hauteur, largeur):
         
     return listOflist
 
+# Position du joueur
 def modifyTableau(list):
-    a = int(input("Choix de la colonne"))-1
-    b = int(input("Choix de la ligne"))-1
+    a = int(input("Choix de la ligne : "))-1
+    b = int(input("Choix de la colonne : "))-1
 
     list[a][b]="0"
 
+# Affichage 
 def affiche(tableau):
     for i in tableau:
         print(i)
 
+# Durée de jeu 
 def demineur():
     liste = _init_(hauteur,largeur)
     pasPerdu=True
@@ -42,37 +60,6 @@ def demineur():
         affiche(liste)
 
 demineur()
-# --------------------------------------
-"""   Nombre de mines """
-# --------------------------------------
-
-def mines(max):
-    nMine = random.randint(2,max)
-    return nMine
-print(mines(5))
-
-
 
 
 #   Position des mines (Tableau invisible)
-"""
-import random
-def _init_(hauteur, largeur):
-
-    listOflist=[]
-
-    for i in range(hauteur):
-        listRow=[]
-
-        for j in range(largeur):
-            listRow.append("x")
-        listOflist.append(listRow)
-        
-
-    return listOflist
-
-liste=_init_(5,5)
-
-for i in liste:
-    print(i)
-"""
